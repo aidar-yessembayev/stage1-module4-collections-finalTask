e154b0fbf4d99b1c8ccdbdf49d66d5c0aea347cf
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-30 15:14:03 +0600

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> result = new HashSet<>();
+
+        if (timetable.size() > 0) {
+
+            for (String i : timetable.keySet()) {
+                for (String j : timetable.get(i)) {
+                    result.add(j);
+                }
+            }
+
+        }
+
+        return result;
     }
 }
